68a0ae8899b087c6d1e77f905a74fe45882dc839
Pauline Foust <pauline.foust@gmail.com>
2023-09-09 21:09:57 -0400

    connecting mongodb

--- server/index.js
+++ server/index.js
@@ -7,4 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(
+    "mongodb+srv://caffeemeow:abc1234@todoapp.g8ieldw.mongodb.net/todoapp?retryWrites=true&w=majority",
+  );
+
 app.listen(3001, () => console.log("listening"));
