8dc0ac2ce9f5bd609b494d8d07f8f6effe245680
Pauline Foust <pauline.foust@gmail.com>
2023-09-11 23:15:18 -0400

    creating cookie and navigate

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,3 +1,9 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
+
 export const Auth = () => {
     return (
     
@@ -12,6 +18,29 @@ export const Auth = () => {
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const [_, setCookies] = useCookies(["access_token"]);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const result = await axios.post("http://localhost:3001/auth/login", {
+            username,
+            password,
+          });
+    
+          setCookies("access_token", result.data.token);
+          window.localStorage.setItem("userID", result.data.userID);
+          navigate("/");
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+
     return (
         <div className="auth-container">
           <form onSubmit={handleSubmit}>
@@ -47,9 +76,23 @@ const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+      alert("Registration Completed! Now login.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
     return (
         <div className="auth-container">
-          <form>
+          <form onSubmit={handleSubmit}>
             <h2>Register</h2>
             <div className="form-group">
               <label htmlFor="username">Username:</label>
@@ -75,4 +118,3 @@ const Register = () => {
       );
     };
 
-    
