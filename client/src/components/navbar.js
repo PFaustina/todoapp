4f5db7550b420195f197c39f2a16210f2d873210
Pauline Foust <pauline.foust@gmail.com>
2023-09-11 23:19:37 -0400

    creating navbar cookie

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 
 
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+   
+
     return <div className="navbar">
         <Link to="/">Home</Link>
         <Link to="/create-todos">Create Todos</Link>
