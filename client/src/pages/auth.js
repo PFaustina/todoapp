cfe734a51f81d7f4482c67b86426f8aa1ebf6567
Pauline Foust <pauline.foust@gmail.com>
2023-09-11 23:03:07 -0400

    creating handleSubmit

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -10,40 +10,69 @@ export const Auth = () => {
 };
 
 const Login = () => {
-    return <div></div>
-}
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+        <div className="auth-container">
+          <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      );
+    };
+    
+
+
 
 const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     return (
-    <div className="auth-container">
-    <form>
-    <h1>Register</h1>
-    <div className="form-group">
-    <label htmlFor="username"> Username </label>
-    <input 
-    type="text" 
-    id="username"
-    value={username}
-    onChange={(event) => setUsername(event.target.value)}/>
-      </div>
-    </form>
-</div>
+        <div className="auth-container">
+          <form>
+            <h2>Register</h2>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      );
+    };
 
-
-<div className="form-group">
-<label htmlFor="password"> Password </label>
-<input 
-type="text" 
-id="password"
-value={password}
- onChange={(event) => setUsername(event.target.value)}/>
-  </div>
-
-<button type="submit">Register</button>
-</form>
-</div>
-    )
-}
+    
