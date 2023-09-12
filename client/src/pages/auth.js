b5ac0bbd38803630f3299a810c0771d6299c91c3
Pauline Foust <pauline.foust@gmail.com>
2023-09-11 22:46:52 -0400

    creating state for login and register

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,7 +1,36 @@
 export const Auth = () => {
-    return <div className="auth-container">
+    return (
+    
+    <div className="auth-container">
         {" "}
         <Login />
         <Register />
     </div>
+    )
 };
+
+const Login = () => {
+    return <div></div>
+}
+
+const Register = () => {
+    return (
+    <div className="auth-container">
+    <form>
+    <h1>Register</h1>
+    <div className="form-group">
+    <label htmlFor="username"> Username </label>
+    <input type="text" id="username"  onChange={(event) => }/>
+      </div>
+    </form>
+</div>
+
+
+<div className="form-group">
+<label htmlFor="password"> Password </label>
+<input type="text" id="password"  onChange={(event) => }/>
+  </div>
+</form>
+</div>
+    )
+}
