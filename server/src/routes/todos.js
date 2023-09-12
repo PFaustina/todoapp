4bf30cfd646d52c7538b091887573a81017ec36b
Pauline Foust <pauline.foust@gmail.com>
2023-09-11 23:51:26 -0400

    creating get route for todo

--- server/src/routes/todos.js
+++ server/src/routes/todos.js
@@ -1,9 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-import { RecipesModel } from "../models/Recipes.js";
-import { UserModel } from "../models/Users.js";
+import { TodosModel } from "../models/Todos.js";
+import { UsersModel } from "../models/Users.js";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+      const response = await TodosModel.find({});
+      res.json(response);
+    } catch (err) {
+      res.json(err);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+  
 
 export { router as todosRouter };
