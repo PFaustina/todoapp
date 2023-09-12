311f906b42a22befee74d264121463a2bf798216
Pauline Foust <pauline.foust@gmail.com>
2023-09-11 23:44:43 -0400

    updating index.js

--- server/src/index.js
+++ server/src/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/todos", todosRouter);
 
 mongoose.connect(
     "mongodb+srv://caffeemeow:abc1234@todoapp.g8ieldw.mongodb.net/todoapp?retryWrites=true&w=majority",
---
+++ server/src/routes/todos.js
@@ -0,0 +1,9 @@
+import express from "express";
+import mongoose from "mongoose";
+import { RecipesModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
+
+const router = express.Router();
+
+
+export { router as todosRouter };
